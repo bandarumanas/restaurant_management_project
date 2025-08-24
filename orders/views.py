50c2fe74999baf7632d73e8abe5c3535af5864fb
Bandaru Manasa <142585724+bandarumanas@users.noreply.github.com>
2025-08-24 18:32:02 +0530

    38203

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    context={"title":"about the restaurant","description":("we're serving fresh and seasonal cuisines.Real taste finds here"),}
+    return render(request,"about.html",context)
