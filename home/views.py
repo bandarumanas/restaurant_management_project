4ee7add0cfad546e33181d977c9cb849d1978e90
Bandaru Manasa <142585724+bandarumanas@users.noreply.github.com>
2025-08-24 19:32:34 +0530

    38202

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    restaurant_name="Oy! Ramen"
+    return render(request,"home.html",{"restaurant_name":Oy! Ramen})
 
