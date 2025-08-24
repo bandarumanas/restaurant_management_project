0be54e348cf1675d8996ba2d945e7c048f2db422
Bandaru Manasa <142585724+bandarumanas@users.noreply.github.com>
2025-08-24 19:58:21 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import home_page
 
-urlpatterns = [ path('homie',views.home_page,name="home")
+urlpatterns = [ path('homie',views.home_page,name="home"),
     
+
 ]
